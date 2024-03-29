a6eea52e01ce7e0dbf87419b04695f7fa2d1f1dc
Laura Cao <andromefingercorn@gmail.com>
2024-03-29 02:57:10 -0400

    added alternating projections to exports

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import AffineProj from "./projections/affine_proj.js";
 import DouglasRachford from "./algorithms/douglas_rachford.js";
+import AlternatingProjections from "./algorithms/alternating_projections.js";
 
 export default {
     AffineProj,
-    DouglasRachford
+    DouglasRachford,
+    AlternatingProjections
 }
