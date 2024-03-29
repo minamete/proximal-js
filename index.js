7547e690a62436d210846beb4851294205f76bc7
Laura Cao <andromefingercorn@gmail.com>
2024-03-29 02:14:10 -0400

    updated to use es6 format

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import AffineProj from "./projections/affine_proj.js";
 import DouglasRachford from "./algorithms/douglas_rachford.js";
 
-module.exports = {
-    AffineProj : AffineProj,
-    DouglasRachford : DouglasRachford
+export default {
+    AffineProj,
+    DouglasRachford
 }
